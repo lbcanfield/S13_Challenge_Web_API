ae58bbb2a754807b937bd95b5b22e870c415b099
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-27 10:15:52 -0600

    /api/projects/projects-middleware.js - created middleware to verify new project /api/projects/projects-router.js - added endpoint to post new project

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,36 @@
-// add middlewares here related to projects
+const PROJECTS = require('./projects-model');
+
+async function validateProjectId(request, response, next) {
+     try {
+          const projectLookUp = await PROJECTS.get(request.params.id)
+          if (!projectLookUp) {
+               response.status(404).json({
+                    message: "Project Not Found"
+               })
+          }
+          else {
+               request.project = projectLookUp;
+               next();
+          }
+     }
+     catch (error) {
+          next(error)
+     }
+}
+
+function validateProject(request, response, next) {
+     if (!request.body.name || !request.body.description) {
+          response.status(400).json({
+               message: "missing required field"
+          })
+     }
+     else {
+          // request.project = { name: name, description: description }
+          next()
+     }
+}
+
+module.exports = {
+     validateProjectId,
+     validateProject
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const PROJECTS = require('./projects-model')
+const PROJECTS = require('./projects-model');
+const {
+     validateProjectId,
+     validateProject
+} = require('./projects-middleware');
 
 const router = express.Router();
 
@@ -12,15 +16,24 @@ router.get('/', (request, response, next) => {
           })
           .catch(next)
 })
-// - [ ] `[GET] /api/projects`
-//   - Returns an array of projects as the body of the response.
-//   - If there are no projects it responds with an empty array.
-// - [ ] `[GET] /api/projects/:id`
-//   - Returns a project with the given `id` as the body of the response.
-//   - If there is no project with the given `id` it responds with a status code 404.
+
+router.get('/:id', validateProjectId, (request, response) => {
+     response.json(request.project)
+})
+
+
+router.post('/', validateProject, (request, response, next) => {
+     PROJECTS.insert(request.body)
+          .then(newProject => {
+               response.status(201).json(newProject)
+          })
+          .catch(next)
+})
 // - [ ] `[POST] /api/projects`
 //   - Returns the newly created project as the body of the response.
 //   - If the request body is missing any of the required fields it responds with a status code 400.
+
+
 // - [ ] `[PUT] /api/projects/:id`
 //   - Returns the updated project as the body of the response.
 //   - If there is no project with the given `id` it responds with a status code 404.
@@ -31,5 +44,14 @@ router.get('/', (request, response, next) => {
 // - [ ] `[GET] /api/projects/:id/actions`
 //   - Returns an array of actions (could be empty) belonging to a project with the given `id`.
 //   - If there is no project with the given `id` it responds with a status code 404.
+
+//Error Handling 
+router.use((error, request, response, next) => {
+     response.status(error.status || 500).json({
+          message: error.message,
+          stack: error.stack
+     })
+})
+
 
 module.exports = router
