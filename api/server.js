152057dbbfcffe38013b0340e467ed859503ad7d
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-27 10:54:23 -0600

    /api/server.js - required the actionsRouter definition - used the actionsRouter on the server /api/actions/actions-router.js - defined generic router - created get endpoint

--- api/server.js
+++ api/server.js
@@ -3,11 +3,13 @@ const server = express();
 const { serverLogger } = require("./server_middleware");
 //Router Imports
 const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
 
 //Server Use Statements
 server.use(express.json());
 server.use(serverLogger);
 server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
